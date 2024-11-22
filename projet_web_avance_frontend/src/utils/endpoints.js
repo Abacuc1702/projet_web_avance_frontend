48384b4d36b59615c1274ba2faa9a4c3fbe6c97f
AGBEDJE OSSOHOU ABACUC <122755978+Abacuc1702@users.noreply.github.com>
2024-11-22 14:29:34 +0000

    Commit de quelques fichiers json

---
+++ projet_web_avance_frontend/src/utils/endpoints.js
@@ -0,0 +1,17 @@
+/** 
+ * 
+ * Ce fichier contient les endpoints 
+ * vers notre backend
+*/
+
+export const API_URL = 'https://backend-5aee.onrender.com'; 
+
+export const LOGIN_URL = API_URL + '/login/'; 
+export const USER_LIST_URL = API_URL+'/users';
+export const USER_URL = API_URL + '/user/';
+export const USER_CREATE_URL = API_URL + '/register/';
+export const PRODUCT_URL = API_URL+'/produit/';
+export const PRODUCT_TYPE_URL = API_URL+'/type-produit/';
+export const PRODUCT_CATEGORY_URL = API_URL+'/categorie-produit/';
+export const COMMANDE_URL = API_URL+'/commandes/';
+export const REAPPROVISIONNEMENT_URL = API_URL+'/reapprovisionnements/';
